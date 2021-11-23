e2d4c245ff289dd66d7d3b7bbfe72395ec7bb831
Sanket Wable <sanketwable123@gmail.com>
2021-11-23 16:43:24 +0530

    messageAPI

--- server/server.go
+++ server/server.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 // Run is used to start the server
@@ -22,8 +23,8 @@ func Run() {
 // Listen is used to make server run on partucular port
 func Listen(port int) {
 	r := router.New()
-	// portt := os.Getenv("PORT")
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), r)
+	portt := os.Getenv("PORT")
+	err := http.ListenAndServe(fmt.Sprintf(":%s", portt), r)
 	if err != nil {
 		log.Fatal("error is : ", err)
 	}
